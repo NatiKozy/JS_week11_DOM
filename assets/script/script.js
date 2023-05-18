9c9e8114dd5e81b7d1d84b029d26efbce75d802d
Nataly Kozyreva <eve30@yandex.ru>
2023-05-18 14:32:43 +0300

    add variables

---
+++ assets/script/script.js
@@ -0,0 +1,13 @@
+let price1 = document.getElementById("price1");
+console.log(price1.innerHTML);
+let price2 = document.getElementById("price2");
+let price3 = document.getElementById("price3");
+let price4 = document.getElementById("price4");
+
+// У нас есть элементы, в которых записаны цены айтимов из корзины. Нужно записать их все в переменные.
+// Взять значение из них (метод innerHTML)
+// Привести значение переменных к числу (вспомним про унарный плюс)
+// Сложить значения переменных
+// Вставить полученное значение в элемент (предварительно найти его и записать в переменную)
+// Создать функцию, которая будет вычитать 20%  от общей стоимости
+// Добавить метод onclick или addEventListener на кнопку ”Использовать купон на 20%”
