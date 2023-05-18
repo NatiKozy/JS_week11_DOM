5afc29bacfcd6cf08f33e4c947efcd4c1fbc0c94
Nataly Kozyreva <eve30@yandex.ru>
2023-05-18 16:19:12 +0300

    js is done

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,34 @@
 let price1 = document.getElementById("price1");
 console.log(price1.innerHTML);
+
 let price2 = document.getElementById("price2");
+console.log(price2.innerHTML);
+
 let price3 = document.getElementById("price3");
+console.log(price3.innerHTML);
+
 let price4 = document.getElementById("price4");
+console.log(price4.innerHTML);
+
+let totalPrice = +price1.innerHTML + +price2.innerHTML + +price3.innerHTML + +price4.innerHTML;
+console.log (totalPrice);
+
+document.getElementById("basket__result").textContent = totalPrice;
+
+function percents(){
+    let sale = 20;
+    let total = totalPrice - (totalPrice / 100 * sale);
+    console.log(total.toFixed(2));
+    document.getElementById("basket__result").textContent = total.toFixed(2);
+}
+
+let button = document.getElementById("basket__sales-btn");
+button.addEventListener('click', percents);
+
+
+
+
+
 
 // У нас есть элементы, в которых записаны цены айтимов из корзины. Нужно записать их все в переменные.
 // Взять значение из них (метод innerHTML)
